53286962d446342fdb71c661b35f43a621ad7571
João André Martins Dias e Silva <joao.silva@marttech.com.br>
2021-01-07 10:39:34 -0300

    Adicionar arquivos de projeto.

---
+++ Alura_Refatoracao_2/Aula_1/Cliente.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Alura_Refatoracao_2.Aula_1
+{
+    class Cliente
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Telefone { get; set; }
+        public string Website { get; set; }
+        public Cliente()
+        {
+
+        }
+
+        public Cliente(int id,string nome, string telefone, string website)
+        {
+            Id = id;
+            Nome = nome;
+            Telefone = telefone;
+            Website = website;
+        }
+        public override string ToString()
+        {
+            return $"{Nome} - {Telefone} - {Website}";
+        }
+    }
+}
---
+++ Alura_Refatoracao_2/Aula_1/Empresa.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace Alura_Refatoracao_2.Aula_1
+{
+    class Empresa
+    {
+        public Empresa()
+        {
+            using (var streamReader = File.OpenText("clientes.csv"))
+            {
+                string linha = string.Empty;
+                while ((linha = streamReader.ReadLine()) != null)
+                {
+                    string[] fields = linha.Split(',');
+                    Cliente cli = new Cliente(int.Parse(fields[0]), fields[1],fields[2],fields[3]);
+                    
+                    Console.WriteLine("Dados do Cliente");
+                    Console.WriteLine("================");
+                    Console.WriteLine("ID: " + cli.Id);
+                    Console.WriteLine("Nome: " + cli.Nome);
+                    Console.WriteLine("Telefone: " + cli.Telefone);
+                    Console.WriteLine("Website: " + cli.Website);
+                    Console.WriteLine("================");
+                }
+            }
+        }
+}
